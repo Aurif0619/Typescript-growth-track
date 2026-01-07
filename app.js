3975a76d2ec8baec90d90872068eb132ef3b44e5
Aurif0619 <muhammadaurifsolangixwave@gmail.com>
2026-01-07 21:55:13 +0500

    Practiced with few variables with examples

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-var message = "Muhammad Aurif";
+var message = "Asslam Alikum";
 console.log(message);
+var name = "Muhammad Aurif";
+console.log(name);
+var num = 1212;
+console.log(num);
+var fals = false;
+console.log(fals);
--- app.ts
+++ app.ts
@@ -1,3 +1,14 @@
-let message: string = "Muhammad Aurif"
+let message: string = "Asslam Alikum"
 
 console.log(message);
+
+let name: string = "Muhammad Aurif"
+console.log(name);
+
+let num: number = 1212
+
+console.log(num);
+
+let fals: boolean = false
+
+console.log(fals);
