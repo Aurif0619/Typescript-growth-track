a38316b2b561b6f91e2fdc941e1580f7bae579e5
Aurif0619 <muhammadaurifsolangixwave@gmail.com>
2026-01-07 22:45:01 +0500

    Learned about Float, object and Function data types

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
-var message = "Asslam Alikum";
-console.log(message);
-var name = "Muhammad Aurif";
-console.log(name);
-var num = 1212;
+// let message: string = "Asslam Alikum"
+// console.log(message);
+// let name: string = "Muhammad Aurif"
+// console.log(name);
+// let num: number = 1212
+// console.log(num);
+// let false: boolean = false
+// console.log(false);
+// Float Datatype
+// let num: number=10
+var num = 10.10;
 console.log(num);
-var fals = false;
-console.log(fals);
+// data types 
+var isLogin = false;
+console.log(isLogin);
+// It's null
+var Language = null;
+console.log(Language);
+// it's not null, null means vacume
+var data = "";
+console.log(data);
+var undefined;
+console.log(undefined);
+// symbol means it crate unique data type
+// Object data types it buil with permitive data types and they hold multiple permitive data types
+// Array collection same data types
+var nameArray = ['120'];
+console.log(nameArray);
+// special data types
+// any, unknown, void, never
+var Y = true;
+// void means it nothing return
+// never mean it never return 
+// advance data types, we write multiple values or data types are hold
+// Function type defines the type of functions
--- app.ts
+++ app.ts
@@ -1,14 +1,61 @@
-let message: string = "Asslam Alikum"
+// let message: string = "Asslam Alikum"
 
-console.log(message);
+// console.log(message);
 
-let name: string = "Muhammad Aurif"
-console.log(name);
+// let name: string = "Muhammad Aurif"
+// console.log(name);
 
-let num: number = 1212
+// let num: number = 1212
 
+// console.log(num);
+
+// let false: boolean = false
+
+// console.log(false);
+
+
+// Float Datatype
+// let num: number=10
+let num: number=10.10
 console.log(num);
 
-let fals: boolean = false
+// data types 
+let isLogin=false
 
-console.log(fals);
+console.log(isLogin);
+
+// It's null
+let Language =null
+console.log(Language);
+
+// it's not null, null means vacume
+let data = "";
+console.log(data);
+
+
+let undefined: undefined
+console.log(undefined);
+
+
+// symbol means it crate unique data type
+
+// Object data types it buil with permitive data types and they hold multiple permitive data types
+
+// Array collection same data types
+
+let nameArray: string[]=['120']
+
+console.log(nameArray);
+
+
+// special data types
+
+// any, unknown, void, never
+let Y:any=true
+
+// void means it nothing return
+// never mean it never return 
+
+// advance data types, we write multiple values or data types are hold
+
+// Function type defines the type of functions
