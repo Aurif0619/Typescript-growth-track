d032d7b4adbdfe2904c4e3192d604bbb1ee5b892
Aurif0619 <muhammadaurifsolangixwave@gmail.com>
2026-02-01 14:47:23 +0500

    Worked on Any_unknown type

---
+++ any_unknown.ts
@@ -0,0 +1,12 @@
+let userInfo = `121`;
+
+console.log(userInfo);
+
+let data: any  = {
+   string: "sa", 21
+  
+  }
+
+  console.log(data);
+  
+
---
+++ output/any_unknown.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let userInfo = `121`;
+console.log(userInfo);
+let data = {
+    string: "sa", 21: 
+};
+console.log(data);
+//# sourceMappingURL=any_unknown.js.map
