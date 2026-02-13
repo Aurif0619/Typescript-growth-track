b7880ea0536643a9753eedfe0fd130259211d899
Aurif0619 <muhammadaurifsolangixwave@gmail.com>
2026-02-13 21:28:29 +0500

    Practiced with counter funtionality

---
+++ output/counter.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let increase = 0;
+let decrease = 1;
+let quantity = increase + -decrease;
+console.log(quantity);
+//# sourceMappingURL=counter.js.map
