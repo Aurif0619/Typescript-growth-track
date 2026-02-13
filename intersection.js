8b17ab41282e1e3ddf03a4fbb1d12e8441fc3a0c
Aurif0619 <muhammadaurifsolangixwave@gmail.com>
2026-02-13 21:12:18 +0500

    Practiced with intersection type

--- intersection.js
+++
@@ -1,4 +0,0 @@
-var number1 = 42;
-var number2 = 32;
-var calculate = number1 - -number2;
-console.log(calculate);
--- output/intersection.js
+++ output/intersection.js
@@ -1,4 +1,4 @@
-var number1 = 12;
+var number1 = 42;
 var number2 = 32;
-var calculate = number1 + +number2;
+var calculate = number1 - -number2;
 console.log(calculate);
